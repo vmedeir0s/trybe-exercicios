b3c25d7a21332c50860d60115f78c8224cb6adff
Vinicius de Medeiros <vmeedeiros@gmail.com>
2022-02-23 18:34:24 -0300

    New Day

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,5 +1,3 @@
-document.getElementById('myInputId').DatePickerX.init();
-
 let botao = document.querySelector('button[type="submit"]');
 
 let form = document.querySelector('form');
@@ -23,3 +21,61 @@ botao.addEventListener('click', function (event) {
     }
   }
 })
+import JustValidate from 'just-validate';
+
+// initialize the validation library
+const validation = new JustValidate('#form', {
+  errorFieldCssClass: 'is-invalid',
+});
+// apply rules to form fields
+validation
+.addField('#name', [
+{
+  rule: 'minLength',
+  value: 3,
+},
+{
+  rule: 'maxLength',
+  value: 30,
+},
+])
+.addField('#email', [
+{
+  rule: 'required',
+  errorMessage: 'Field is required',
+},
+{
+  rule: 'email',
+  errorMessage: 'Email is invalid!',
+},
+])
+.addField('#password', [
+{
+  rule: 'password',
+},
+])
+.addField('#message', [
+{
+  validator: (value) => {
+    return value[0] === '!';
+  },
+},
+])
+.addField('#consent_checkbox', [
+{
+  rule: 'required',
+},
+])
+.addField('#favorite_animal_select', [
+{
+  rule: 'required',
+},
+])
+.addRequiredGroup(
+'#read_terms_checkbox_group',
+'You should select at least one communication channel'
+)
+.addRequiredGroup('#communication_radio_group')
+.onSuccess((event) => {
+console.log('Validation passes and form submitted', event);
+});
