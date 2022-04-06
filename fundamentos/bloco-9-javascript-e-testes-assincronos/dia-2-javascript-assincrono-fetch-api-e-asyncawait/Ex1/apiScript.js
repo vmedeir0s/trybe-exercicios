9274144c24a6ae15a042614375424e70ad1530d0
Vinicius de Medeiros <vmeedeiros@gmail.com>
2022-04-06 16:57:46 -0300

    Correção

---
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/Ex1/apiScript.js
@@ -0,0 +1,21 @@
+// apiScript.js     
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector('#jokeContainer').innerText = data.joke;
+    })
+    .catch((error) => console.log(error));
+    
+};
+
+
+
+window.onload = () => fetchJoke();
